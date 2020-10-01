c729e9674a115b2888df796582985adf10e579f9
antrapovichs <antrapovichs@gmail.com>
2020-10-01 14:03:19 +0300

    First commit.

---
+++ Assets/Scripts/Fier.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using UnityEngine;
+
+public class Fier : MonoBehaviour
+{
+    public float bounceSpeed;
+    public float fallForce;
+    public float stiffness;
+
+    private MeshFilter _meshFilter;
+    private Mesh _mesh;
+
+    private JellyVertex[] _jellyVertices;
+    private Vector3[] _currentMeshVertices;
+
+    private void Start()
+    {
+        _meshFilter = GetComponent<MeshFilter>();
+        _mesh = _meshFilter.mesh;
+
+        GetVertices();
+    }
+
+    private void GetVertices()
+    {
+        _jellyVertices = new JellyVertex[_mesh.vertices.Length];
+        _currentMeshVertices = new Vector3[_mesh.vertices.Length];
+
+        for (int i = 0; i < _mesh.vertices.Length; i++)
+        {
+            _jellyVertices[i] = new JellyVertex(i, _mesh.vertices[i], _mesh.vertices[i], Vector3.zero );
+            _currentMeshVertices[i] = _mesh.vertices[i];
+        }
+    }
+
+    private void Update()
+    {
+        UpdateVertices();
+    }
+
+    private void UpdateVertices()
+    {
+        for (var i = 0; i < _jellyVertices.Length; i++)
+        {
+            _jellyVertices[i].UpdateVelocity(bounceSpeed);
+            _jellyVertices[i].Settle(stiffness);
+
+            _jellyVertices[i].currentVertexPosition += _jellyVertices[i].currentVelocity * Time.deltaTime;
+            _currentMeshVertices[i] = _jellyVertices[i].currentVertexPosition;
+        }
+
+        _mesh.vertices = _currentMeshVertices;
+        _mesh.RecalculateBounds();
+        _mesh.RecalculateNormals();
+        _mesh.RecalculateTangents();
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        ContactPoint[] collisionPoints = other.contacts;
+
+        for (int i = 0; i < collisionPoints.Length; i++)
+        {
+            var inputPoint = collisionPoints[i].point + (collisionPoints[i].point * .1f);
+            ApplyPressureToPoint(inputPoint, fallForce);
+        }
+    }
+
+    public void ApplyPressureToPoint(Vector3 point, float pressure)
+    {
+        for (int i = 0; i < _jellyVertices.Length; i++)
+        {
+            _jellyVertices[i].ApplyPressure(transform, point, pressure);
+        }
+    }
+}
---
+++ Assets/Scripts/JellyVertex.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class JellyVertex : MonoBehaviour
+{
+    public int vertexIndex;
+
+    public Vector3 initialVertexPosition;
+    public Vector3 currentVertexPosition;
+    public Vector3 currentVelocity;
+
+    public JellyVertex(int _vertexIndex, Vector3 _initialVertexPosition, Vector3 _currentVertexPosition, Vector3 _currentVelocity)
+    {
+        vertexIndex = _vertexIndex;
+        initialVertexPosition = _initialVertexPosition;
+        currentVertexPosition = _currentVertexPosition;
+        currentVelocity = _currentVelocity;
+    }
+
+    public Vector3 GetCurrentDisplacement()
+    {
+        return currentVertexPosition - initialVertexPosition;
+    }
+
+    public void UpdateVelocity(float _bounceSpeed)
+    {
+        currentVelocity = currentVelocity - GetCurrentDisplacement() * _bounceSpeed * Time.deltaTime;
+    }
+
+    public void Settle(float _stiffness)
+    {
+        currentVelocity *= 1f - _stiffness * Time.deltaTime;
+        int [] array = {1,10,20,30,40,50};
+        var sorted = array.OrderByDescending(x => x);
+        var item1 = sorted.ElementAt(0);
+        var item2 = sorted.ElementAt(1);
+        var item3 = sorted.ElementAt(2);
+        
+        
+        int max = array[0];
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (max < array[i])
+            {
+                max = array[i];
+            }
+        }
+        
+    }
+
+    public void ApplyPressure(Transform _transform, Vector3 _position, float _pressure)
+    {
+        Vector3 distanceVerticePoint = currentVertexPosition - _transform.InverseTransformPoint(_position);
+        var adapterPressure = _pressure / (1f + distanceVerticePoint.sqrMagnitude);
+        var velocity = adapterPressure * Time.deltaTime;
+        currentVelocity += distanceVerticePoint.normalized * velocity;
+    }
+}
